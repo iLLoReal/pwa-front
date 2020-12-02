c719f22df2556fe2ab2b4f02bf66e70a1d770af6
olivier <olivier.laffon@epitech.eu>
2020-12-02 19:07:36 +0100

    cra-append-sw and serviceWorker integration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { subscribeUser } from './subscription';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +13,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
+
+subscribeUser()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
